cf57dfcb97c8a491109ac320680f406a3e5870f6
Thomas <thomaskim1995@gmail.com>
2023-09-16 02:30:00 -0700

    fixed data ingestion

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from src.mlProject import logger
+from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info('Logging test')
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
